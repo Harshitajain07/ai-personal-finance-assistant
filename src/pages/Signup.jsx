c7c3ba77fe41b8cb8c4dc514202f2d07092a5ed6
Harshitajain07 <harshitajain178@gmail.com>
2026-06-15 14:26:36 +0530

    Fix signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom"; // ADD THIS
 import { auth } from "../firebase/firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
 function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const navigate = useNavigate(); // ADD THIS
 
   const handleSignup = async () => {
     try {
@@ -15,6 +18,9 @@ function Signup() {
       );
 
       alert("Account Created Successfully!");
+
+      navigate("/login"); // ADD THIS HERE
+
     } catch (error) {
       alert(error.message);
     }
@@ -27,12 +33,14 @@ function Signup() {
       <input
         type="email"
         placeholder="Email"
+        value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
 
       <input
         type="password"
         placeholder="Password"
+        value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
 
